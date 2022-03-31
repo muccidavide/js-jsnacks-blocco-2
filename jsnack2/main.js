9fe32b58cb5c0dfbc892f8f21272b28fefbb9c5f
muccidavide <davidemucci0804@gmail.com>
2022-03-31 14:18:05 +0200

    complete jsnack2

---
+++ jsnack2/main.js
@@ -0,0 +1,14 @@
+/* Snack2
+Inserisci un numero, se è pari stampa il numero,
+se è dispari stampa il numero successivo.
+ */
+
+
+
+let numberUser = parseInt(prompt('scegli un numero'));
+
+if (numberUser % 2 === 0){
+    console.log(`il numero ${numberUser} è pari`);
+} else {
+    console.log(`il numero successivo è ${numberUser + 1}`);
+}
